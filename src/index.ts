c19bb0818754da23e6bb02c9d374432ec17333d0
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-28 19:33:01 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import mainRoutes from './Routes/mainRoutes'
+import globalError from './middleware/errorMiddleware'
+import { AppError } from './utils/appError';
 
 
 
@@ -11,6 +13,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/api', mainRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find this route ${req.originalUrl}`, 400));
+});
+
+app.use(globalError);
 
 app.listen(3000, () => {
     console.log('Server up and running on port: 3000');
