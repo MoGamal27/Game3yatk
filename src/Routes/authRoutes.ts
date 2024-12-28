a56a04b746f633375beb4668319e2d146920add8
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-28 19:33:28 +0200

    add validators

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { signUp, login } from "../Controllers/authController";
+import { signupValidator, signinValidator } from "../utils/validator/authValidator";
 import express from "express";
 const router = express.Router();
 
-router.post("/signup", signUp);
-router.post("/login", login);
+router.post("/signup", signupValidator ,signUp);
+router.post("/login", signinValidator,login);
 
 export default router;
