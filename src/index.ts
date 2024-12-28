51e22ae04ce5694e26bc81011b62285340ec96a0
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-28 16:51:47 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import mainRoutes from './Routes/mainRoutes'
 
 
 
@@ -7,6 +7,10 @@ import express from 'express'
 
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api', mainRoutes);
 
 app.listen(3000, () => {
     console.log('Server up and running on port: 3000');
