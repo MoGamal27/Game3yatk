4d1c61a4e35e2184d42ab6f33694df7a751308e3
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-31 18:30:08 +0200

    Update mainRoutes.ts

--- src/Routes/mainRoutes.ts
+++ src/Routes/mainRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import  authRoutes  from "./authRoutes";
 import circleRoutes from "./circleRoutes";
+import circleMemberRoutes from "./circleMemberRoutes";
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/circle", circleRoutes);
+router.use("/circleMember", circleMemberRoutes);
 
 export default router;
