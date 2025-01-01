1dcd452396492e3be237041470e5e96c94727100
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-01-01 18:07:51 +0200

    feat(Circle Member): add endpoint avaliable Order

--- src/Routes/circleMemberRoutes.ts
+++ src/Routes/circleMemberRoutes.ts
@@ -1,8 +1,9 @@
-import { createCircleMember } from "../Controllers/circleMemberController";
+import { createCircleMember, getAvailablePayoutOrder } from "../Controllers/circleMemberController";
 import { circleMemberValidator } from "../utils/validator/circleMemberValidator";
 import { Router } from "express";
 const router = Router();
 
 router.post("/", circleMemberValidator ,createCircleMember);
+router.get("/available-payout-order/:circleId", getAvailablePayoutOrder);
 
 export default router;
